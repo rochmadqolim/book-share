d5624fc5989b3d84263d3c32d0d93cdc3e0d514f
rochmadqolim <rochmadcholim@gmail.com>
2023-10-14 14:27:31 +0700

    create search by category and title feature

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class PublicController extends Controller
 {
-    public function index(){
-        $books = Book::all();
-        return view('bookList', ['books'=>$books]);
+    public function index(Request $request){
+        $categories = Category::all();
+
+        $books = Book::when($request->category, function ($query) use ($request) {
+            $query->whereHas('categories', function ($q) use ($request) {
+                $q->where('categories.id', $request->category);
+            });
+        })
+        ->when($request->title, function ($query) use ($request) {
+            $query->where('title', 'like', '%' . $request->title . '%');
+        })
+        ->get();
+        
+        return view('bookList', ['books'=>$books, 'categories'=>$categories]);
     }
 }
--- resources/views/bookList.blade.php
+++ resources/views/bookList.blade.php
@@ -4,6 +4,25 @@
 
 @section('content')
 
+<form action="" method="GET">
+    <div class="row">
+        <div class="col-12 col-sm-6">
+            <select name="category" id="category" class="form-control">
+                <option value="">Select Category</option>
+                @foreach ($categories as $item)
+                    <option value="{{ $item->id }}">{{ $item->name }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="col-12 col-sm-6">
+            <div class="input-group mb-3">
+                <input type="text" name="title" class="form-control" placeholder="Book Title">
+                <button class="btn btn-primary">Search</button>
+            </div>
+        </div>
+    </div>
+</form>
+
 <div class="my-5">
     <div class="row">
         @foreach ($books as $item)
