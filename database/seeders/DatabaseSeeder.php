7c349bf03a3288d63a98b1cbfa4db9eb06438443
rochmadqolim <rochmadcholim@gmail.com>
2023-10-15 09:43:32 +0700

    create user admin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +17,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Schema::disableForeignKeyConstraints();
+        User::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        DB::table('users')->insert([
+            'username' => 'admin',
+            'slug' => 'admin',
+            'password' => Hash::make('rahasia'),
+            'phone' => '0898989',
+            'address' => 'Malang',
+            'status' => 'active',
+            'role_id' => 1,
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
